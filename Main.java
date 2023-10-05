a442273aa99521a74c1384b4de8493a29ea6ca23
YashJava <yashdeepkavade77@gmail.com>
2023-10-05 11:07:14 +0530

    added student class

--- Main.java
+++ Main.java
@@ -7,5 +7,7 @@ public class Main{
         System.out.println(new Date());
         System.out.println("Hello");
         System.out.println("Addded one more line");
+        Student stu=new Student();
+        stu.sayHello();
     }
 }
